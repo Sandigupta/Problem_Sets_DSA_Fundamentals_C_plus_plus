d7dce0d95e3f9e9f7a1cae558911fee0dfad2a44
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2024-01-04 22:21:50 +0530

    Add files via upload

---
+++ Recurssion_problem_6_lecture/sir_method_Finding_x_in_a_array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool f(int *arr, int idx, int x, int n)
+{
+    if (idx == n)
+    {
+        return false;
+    }
+    
+    return (arr[idx] == x) || f(arr, idx + 1, x, n);
+}
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int idx = 0;
+    bool result = f(arr, idx, x, n);
+    if (result)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}

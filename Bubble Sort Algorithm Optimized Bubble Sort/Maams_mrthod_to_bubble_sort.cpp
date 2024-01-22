c09f79923c17e0e9ace8645215f7ce3c6e9e31c6
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2024-01-22 22:54:24 +0530

    Add files via upload

---
+++ Bubble Sort Algorithm Optimized Bubble Sort/Maams_mrthod_to_bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble(vector<int> &v){
+    int n = v.size();
+
+    for (int i = 0; i < n - 1;i++){
+        for (int j = 0; j < n - i - 1;j++){
+            if(v[j]>v[j+1])
+                swap(v[j], v[j + 1]);
+        }
+    }
+    return;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+     for (int i = 0; i < n; i++)
+    {
+        cin>>v[i];
+    }
+
+    bubble(v);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    
+}
